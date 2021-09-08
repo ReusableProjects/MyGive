8eac9de89499747c51b07fd1ba6aa1a20473b94f
tlfzgjadyd <tlfzgjadyd@naver.com>
2021-09-08 23:01:09 +0900

    팝업창

--- App/CardHistoryManagement/script/cardHistory.js
+++ App/CardHistoryManagement/script/cardHistory.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
-    $('#organizaionHistory li').click(function(){
-        window.open($(this).attr('data'));
+    var homepageFlag=false;
+    $('.homepage').click(function(){
+        homepageFlag=true;
+        window.open($(this).attr('data')).setTimeout(function(){
+           homepageFlag=false;
+        }, 10);
+
+    })
+    $('#organizationHistoryDiv li').click(function(){
+        if(homepageFlag==false)
+        {
+            $('#popupBackground').css('visibility', 'visible');
+            $('#popupContent').css('visibility', 'visible');
+        }
+    })
+    $('#historyPopup #popupBackground, #quit').click(function(){
+        $('#popupContent').css('visibility', 'hidden');
+        $('#popupBackground').css('visibility', 'hidden');
     })
    
 })
